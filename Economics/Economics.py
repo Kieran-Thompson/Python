7f56e4f9015f1f219f3d935abc96a3ca80ceef7c
Kieran Thompson <35673859+Kieran-Thompson@users.noreply.github.com>
2020-09-03 12:37:57 +0100

    Fix broken elements and links.

--- Economics/Economics.py
+++ Economics/Economics.py
@@ -13,7 +13,7 @@ from twitterScript import *
 def windowSettings(window):
 
     ##set the window size and app title text
-    window.geometry('1100x500+0+0')
+    window.geometry('1200x500+0+0')
     window.title("Economics")
 
     return window
@@ -31,9 +31,9 @@ def setBasicLabel(window, text_val, col_val, row_val):
 def screenShot():
 
     ##simulate the keypress, up and down  
-    im = pyautogui.screenshot(region=(10,150, 580, 270))
+    im = pyautogui.screenshot(region=(15, 150, 760, 270))
 
-    im.save('screenshot.png')
+    im.save('screenshot-figures.png')
 
     ##post to twitter
     twitterMain()
@@ -42,35 +42,23 @@ def screenShot():
 ##given the window, place the economic indicators on the screen
 def indicators(window):
 
-    ##get the value of CPI place on screen as a label
-    cpi_text = EcoScrape.getDataFromWeb("https://www.bankofengland.co.uk/monetary-policy/inflation","p","stat-figure")
-    lblCPIValue = setBasicLabel(window,"The value of CPI is : " + cpi_text , 0, 2)
-    
+    ##get the value of CPIH and place on screen as a label
+    cpih_text = EcoScrape.getDataFromWeb("https://www.ons.gov.uk/economy/inflationandpriceindices","p","stand-out")[1:-3]
+    lblCPIHValue = setBasicLabel(window, "The value of CPIH is " + cpih_text + "%", 0, 2)
+
 
     btn_screeenShot = tk.Button(window, text =" Screenshot and Post ", command = screenShot)
     btn_screeenShot.grid(column=1, row=2)
 
 
-    
     ##get the value of the Interest rate, place on screen as a label
-    interestRate_text = EcoScrape.getDataFromWeb("https://www.bankofengland.co.uk/monetary-policy/the-interest-rate-bank-rate","p","stat-figure")
-    lblIRValue = setBasicLabel(window,"The interest rate is : " + interestRate_text , 0, 3)
-   
+    interestRate_text = EcoScrape.getDataFromWeb("https://www.bankofengland.co.uk/boeapps/database/Bank-Rate.asp","p","stat-figure")
+    lblIRValue = setBasicLabel(window,"The interest rate is " + interestRate_text , 0, 3)
+
 
     ##get unemployment value from the web
-    ##second value in the retrived list
-    unemployment_text = EcoScrape.getDataFromWeb("https://www.ons.gov.uk/employmentandlabourmarket/peoplenotinwork/unemployment","p","stand-out")[1]
-    lbl_UE_RateValue = setBasicLabel(window,"The unemployment rate is : " + unemployment_text + " %" , 0, 4)
-    
-        
-    ##get the value of economic growth value from the web
-    economicGrowth_paragraph = EcoScrape.getDataFromWeb("https://en.wikipedia.org/wiki/Economy_of_the_United_Kingdom","div","plainlist")
-    
-
-    ##create list and get the value, second value
-    economicGrowthList = economicGrowth_paragraph.split(" ")
-    lblEGrowth = setBasicLabel(window,"The rate of economic growth is: " + economicGrowthList[1], 0, 5)
-
+    unemployment_text = EcoScrape.getDataFromWeb("https://www.ons.gov.uk/employmentandlabourmarket/peoplenotinwork/unemployment","p","stand-out")[1:-3]
+    lbl_UE_RateValue = setBasicLabel(window,"The unemployment rate is " + unemployment_text + " %" , 0, 4)
 
 
 
@@ -92,7 +80,7 @@ def main():
     indicators(window)
 
     space = setBasicLabel(window,"", 0, 7)
-    note = setBasicLabel(window,"! - Please take down a note of these economic indicators. ",0,8)
+    note = setBasicLabel(window,"! - Please take down a note of these economic indicators.   ",0,8)
     
     ##run the window object 
     window.mainloop()
